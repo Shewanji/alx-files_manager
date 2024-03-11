0d3d55bd4bdaa66ac429ab784d165cd5d8ce8d93
Shewanji <marrionwanjiru26@gmail.com>
2024-03-11 10:56:40 +0000

    First API

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = require('./routes/index');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
